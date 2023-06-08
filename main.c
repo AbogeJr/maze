32a2f7cb4ea7a4b835e3879fb028f36922cffcf1
AbogeJr <junioraboge@gmail.com>
2023-06-08 17:01:53 +0300

    cleanup

--- main.c
+++ main.c
@@ -1,11 +1,52 @@
-#include <SDL2/SDL.h>
+#include "SDL2/SDL.h"
 
-int main(int argc, char **argv)
-{
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window * win = SDL_CreateWindow("Aboge Window", 30, 30, 600, 500, SDL_WINDOW_SHOWN);
-	SDL_Surface * screen = SDL_GetWindowSurface(win);
-	SDL_Delay(5000);
-	SDL_DestroyWindow(win);
-	SDL_Quit();
+int main(int argc, char *argv[]){
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+    int quit = 0;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+
+    while (!quit){
+        SDL_WaitEvent(&event);
+        switch (event.type){
+            case SDL_QUIT:
+                quit = 1;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym){
+                    case SDLK_ESCAPE:
+                        quit = 1;
+                        break;
+                    case SDLK_UP:
+                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                        SDL_RenderDrawPoint(renderer, 320, 240);
+                        break;
+                    case SDLK_DOWN:
+                        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+                        SDL_RenderDrawPoint(renderer, 320, 240);
+                        break;
+                    case SDLK_LEFT:
+                        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+                        SDL_RenderDrawPoint(renderer, 320, 240);
+                        break;
+                    case SDLK_RIGHT:
+                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                        SDL_RenderDrawPoint(renderer, 320, 240);
+                        break;
+                }
+                break;
+
+        }
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
