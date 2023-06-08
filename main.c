478e6d34607080d788a5acee56e4904930f83258
AbogeJr <junioraboge@gmail.com>
2023-06-09 00:11:20 +0300

    cleanup

--- main.c
+++ main.c
@@ -1,50 +1,42 @@
 #include "SDL2/SDL.h"
+#include <stdio.h>
+#include <stdbool.h>
 
-int main(int argc, char *argv[]){
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Event event;
-    int quit = 0;
+#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 640
+
+
+int main(int argc, char* argv[])
+{
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Event ev;
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
-    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+    bool isRunning = true;
 
-    while (!quit){
-        SDL_WaitEvent(&event);
-        switch (event.type){
-            case SDL_QUIT:
-                quit = 1;
-                break;
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym){
-                    case SDLK_ESCAPE:
-                        quit = 1;
-                        break;
-                    case SDLK_UP:
-                        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-                        SDL_RenderDrawPoint(renderer, 320, 240);
-                        break;
-                    case SDLK_DOWN:
-                        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-                        SDL_RenderDrawPoint(renderer, 320, 240);
-                        break;
-                    case SDLK_LEFT:
-                        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-                        SDL_RenderDrawPoint(renderer, 320, 240);
-                        break;
-                    case SDLK_RIGHT:
-                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                        SDL_RenderDrawPoint(renderer, 320, 240);
-                        break;
-                }
-                break;
+    SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
 
+    while(isRunning)
+    {
+        while(SDL_PollEvent(&ev))
+        {
+            if(ev.type == SDL_QUIT)
+            {
+                isRunning = false;
+            } else {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderClear(renderer);
+
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+
+                SDL_RenderPresent(renderer);
+            }
         }
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
     }
 
+   
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
