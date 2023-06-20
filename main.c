f04431d3a7ba4f559c4a0e06ec45871d33dca9fc
AbogeJr <junioraboge@gmail.com>
2023-06-20 23:02:48 +0300

    player movement

--- main.c
+++ main.c
@@ -8,12 +8,27 @@
 #define SCREEN_WIDTH 640
 #define MAZE_WIDTH 8
 #define MAZE_HEIGHT 8
+#define NUM_WALLS 300
+#define PI 3.14159265
 
 
 
-float playerX = 3.0f;
-float playerY = 3.0f;
-float playerAngle = 0.0f;
+typedef struct {
+    float x, y;
+    float dx, dy;
+    float angle;
+} Player;
+
+typedef struct {
+    int x1, y1;  // Starting point of the wall
+    int x2, y2;  // Ending point of the wall
+} Wall;
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Event ev;
+Player player = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0, 0.0f};
+
 
 // Maze representation (dummy data)
 int maze[MAZE_HEIGHT][MAZE_WIDTH] = {
@@ -21,7 +36,7 @@ int maze[MAZE_HEIGHT][MAZE_WIDTH] = {
     {1, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1}
@@ -31,67 +46,32 @@ const int numRays = SCREEN_WIDTH;  // Number of rays to cast
 const float FOV = 60.0f;
 
 
-// Perform raycasting and rendering
-void raycastAndRender(SDL_Renderer* renderer)
-{
-    for (int rayIndex = 0; rayIndex < numRays; ++rayIndex)
-    {
-        // Calculate ray angle
-        float rayAngle = playerAngle - (FOV / 2) + (rayIndex * (FOV / numRays));
+void renderWalls(Wall* walls, int numWalls) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Set the wall color (white)
+    SDL_RenderClear(renderer);  // Clear the renderer
 
-        // Perform raycasting calculations
-        float rayX = playerX;
-        float rayY = playerY;
-        float rayDirectionX = sin(rayAngle);
-        float rayDirectionY = cos(rayAngle);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // Set the background color (black)
+    SDL_RenderFillRect(renderer, NULL);  // Fill the background
 
-        int hit = 0;
-        while (!hit)
-        {
-            rayX += rayDirectionX;
-            rayY += rayDirectionY;
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Set the wall color (white)
 
-            int gridX = (int)rayX;
-            int gridY = (int)rayY;
+    Wall wall = walls[0];
+    for (int i = 0; i < numWalls; i++) {
 
-            if (gridX < 0 || gridX >= MAZE_WIDTH || gridY < 0 || gridY >= MAZE_HEIGHT)
-            {
-                hit = 1;
-                break;
-            }
-
-            if (maze[gridY][gridX] == 1)
-            {
-                hit = 1;
-
-                // Calculate distance to wall
-                float distanceToWall = sqrt((rayX - playerX) * (rayX - playerX) + (rayY - playerY) * (rayY - playerY));
-
-                // Calculate wall height
-                int wallHeight = (int)(SCREEN_HEIGHT / distanceToWall);
-
-                // Calculate wall color based on distance
-                int wallColor = 255 - (int)(255 * distanceToWall / (float)SCREEN_WIDTH);
-
-                // Set wall color
-                SDL_SetRenderDrawColor(renderer, wallColor, wallColor, wallColor, 255);
-
-                // Render the wall column on the screen
-                SDL_RenderDrawLine(renderer, rayIndex, (SCREEN_HEIGHT - wallHeight) / 2, rayIndex, (SCREEN_HEIGHT + wallHeight) / 2);
-
-                break;
-            }
-        }
+        SDL_RenderDrawLine(renderer, wall.x1+=5, wall.y1, wall.x2+=5, wall.y2);
     }
+
+    SDL_RenderPresent(renderer);  // Update the screen
 }
 
+void renderPlayer(Player* player) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Set the player color (white)
+
+    SDL_RenderDrawLine(renderer, player->x, player->y, player->x + 10 * cos(player->angle), player->y + 10 * sin(player->angle));
+}
 
 int main(int argc, char* argv[])
 {
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
-    SDL_Event ev;
-
     SDL_Init(SDL_INIT_VIDEO);
     bool isRunning = true;
 
@@ -107,36 +87,51 @@ int main(int argc, char* argv[])
             } else if (ev.type == SDL_KEYDOWN)
             {
                 // Handle player movement and rotation
-                switch (ev.key.keysym.sym)
+                if (ev.key.keysym.sym == SDLK_UP)
                 {
-                    case SDLK_UP:
-                        // Move player forward
-                        playerX += sin(playerAngle) * 0.1;
-                        playerY += cos(playerAngle) * 0.1;
-                        break;
-                    case SDLK_DOWN:
-                        // Move player backward
-                        playerX -= sin(playerAngle) * 0.1;
-                        playerY -= cos(playerAngle) * 0.1;
-                        break;
-                    case SDLK_LEFT:
-                        // Rotate player left
-                        playerAngle -= 0.1;
-                        break;
-                    case SDLK_RIGHT:
-                        // Rotate player right
-                        playerAngle += 0.1;
-                        break;
-                    default:
-                        break;
+                    player.angle -= 0.1;
+                    if(player.angle < 0) {player.angle += 2 * PI;}
+                    player.dx = 5 * cos(player.angle);
+                    player.dy = 5 * sin(player.angle);
+                    renderPlayer(&player);
                 }
+                if (ev.key.keysym.sym == SDLK_DOWN)
+                {
+                    player.angle += 0.1;
+                    if(player.angle >= 2 * PI) player.angle -= 2 * PI;
+                    player.dx = 5 * cos(player.angle);
+                    player.dy = 5 * sin(player.angle);
+                    renderPlayer(&player);
+                }
+                if (ev.key.keysym.sym == SDLK_RIGHT)
+                {
+                    player.x -= player.dx;
+                    player.y -= player.dy;
+                    renderPlayer(&player);
+                }
+                if (ev.key.keysym.sym == SDLK_LEFT)
+                {
+                    player.x += player.dx;
+                    player.y += player.dy;
+                    renderPlayer(&player);
+                }
+                
             }
-            SDL_SetRenderDrawColor(renderer, 0, 50, 0, 255);
+            SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
             SDL_RenderClear(renderer);
-
-            // Perform raycasting and rendering
-            raycastAndRender(renderer);
-
+             // Create your walls
+            // Wall walls[NUM_WALLS] = {
+            //     {100, 100, 100, 300},
+            //     {101, 100, 101, 300},
+            //     {102, 100, 102, 300},
+            //     {103, 100, 103, 300},
+            //     {104, 100, 104, 300},
+            //     {105, 100, 106, 300},
+               
+            // };
+            renderPlayer(&player);
+            // Render the walls
+            // renderWalls(walls, NUM_WALLS);
             // Update the screen
             SDL_RenderPresent(renderer);
         }
